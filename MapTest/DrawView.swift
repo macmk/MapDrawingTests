db281b9145dcf8500966dfa56aec550a103a5602
Maciej Koziel <>
2018-01-31 14:24:38 +0100

    line approximation

--- MapTest/DrawView.swift
+++ MapTest/DrawView.swift
@@ -61,8 +61,8 @@ class DrawView: UIView {
     }
     
     private func start(gestureRecognizer: UIPanGestureRecognizer) {
-        points = [CGPoint]()
         lastPosition = gestureRecognizer.location(in: gestureRecognizer.view)
+        points = [lastPosition]
         
         path = UIBezierPath()
         path.lineWidth = 2.0
@@ -79,9 +79,40 @@ class DrawView: UIView {
     }
     
     private func end(gestureRecognizer: UIPanGestureRecognizer) {
-        delegate?.drawView(view: self, didCompletedDrawing: points)
+        guard points.count > 2 else {
+            return
+        }
+        
+        var startIndex = 0
+        var endIndex = 1
+        var newPoints = [points[startIndex]]
+        while endIndex < points.count - 1 {
+            let dist = distance(lineStart: points[startIndex], lineEnd: points[endIndex], point: points[endIndex + 1])
+            print(dist)
+            
+            if dist > 0.5 {
+                newPoints.append(points[endIndex])
+                startIndex = endIndex
+            }
+            
+            endIndex += 1
+        }
+        newPoints.append(points.last!)
+        delegate?.drawView(view: self, didCompletedDrawing: newPoints)
         
         path = UIBezierPath()
         setNeedsDisplay()
+    }
+    
+    // Distance between line and point: https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+    private func distance(lineStart: CGPoint, lineEnd: CGPoint, point: CGPoint) -> Double {
+        let x0 = point.x, y0 = point.y
+        let x1 = lineStart.x, y1 = lineStart.y
+        let x2 = lineEnd.x, y2 = lineEnd.y
+        
+        let numerator =  abs((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1)
+        let denominator = sqrt(pow(y2 - y1, 2) + pow(x2 - x1, 2))
+        
+        return denominator == 0.0 ? 0.0 : Double(numerator / denominator)
     }
 }
